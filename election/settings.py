59f6c2ab402d626451b2a4e34dddbe5cc76251f2
lexysticks <olemmanuel@1528>
2026-01-18 11:48:20 +0100

    restricting core to trusted source

--- election/settings.py
+++ election/settings.py
@@ -170,11 +170,11 @@ REST_FRAMEWORK = {
 # ==============================
 
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173",      # dev frontend
-#     "https://voteng.live",    # production frontend
-# ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",      # dev frontend
+    "https://voteng.live",    # production frontend
+]
+# CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
